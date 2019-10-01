fe1c521454999a07c51427d9ba3036d23fd3d376
Adrian <Adrianrastegari@yahoo.se>
2019-10-01 12:18:24 +0200

    kjg

--- Hemläxav36/src/prog2/Sverige.java
+++ Hemläxav36/src/prog2/Sverige.java
@@ -12,73 +12,264 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-public class Sverige extends Application implements EventHandler<ActionEvent>{
+public class Sverige extends Application implements EventHandler<ActionEvent> {
 
-	Button knappen;
+	static Button sverige;
+	static Button schewiz;
+	static Button frankrike;
+	static Button tyskland;
+	static Stage stage;
 
-	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		launch(args);
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-	      
-		knappen = new Button("Hej");
-		knappen.setOnAction(this);
+		primaryStage = stage;
+		
+		sverige.setOnAction(this);
+		tyskland.setOnAction(this);
+		sverige.setOnAction(this);
+		schewiz.setOnAction(this);
+		Group knappar = new Group(sverige, tyskland, sverige,schewiz);
+		sverige.setTranslateX(100);
+		sverige.setTranslateX(0);
+		schewiz.setTranslateX(200);
+		tyskland.setTranslateX(300);
+		Scene scene = new Scene(knappar, 600, 300);
+		stage.setScene(scene);
+
+		stage.show();
 		
 		
-	      Rectangle rec = new Rectangle();
+		
+	}
 
- 
-	      rec.setHeight(300);
-	      rec.setWidth(600);
-	      rec.setFill(Color.BLUE);
-	      
-	      
-	      Rectangle recgul1 = new Rectangle();
-	      
-	      recgul1.setHeight(300);
-	      recgul1.setWidth(58);
-	      recgul1.setFill(Color.YELLOW);
-	      recgul1.setX(150);
-	      
-	      
-	      
-	      Rectangle recgul2 = new Rectangle();
-	      
-	      recgul2.setHeight(58);
-	      recgul2.setWidth(600);
-	      recgul2.setFill(Color.YELLOW);
-	      recgul2.setY(130);
-	      
-	      
-	      
-	      
-	      HBox box = new HBox(knappen);
+	public static void tyskland() {
+		Rectangle rec = new Rectangle();
+		rec.setTranslateY(50);
+		rec.setHeight(100);
+		rec.setWidth(600);
+		rec.setFill(Color.BLACK);
 
-	      
-	      Group root = new Group(rec, recgul1, recgul2);
+		Rectangle recgul1 = new Rectangle();
+		rec.setTranslateY(50);
+		recgul1.setHeight(100);
+		recgul1.setWidth(600);
+		recgul1.setFill(Color.RED);
+		recgul1.setY(100);
 
-	      box.getChildren().add(root);
-	      
-	      Scene scene = new Scene(box, 600, 300);
-	     
-	      primaryStage.setTitle("Drawing a Sweden"); 
-	      
-	      primaryStage.setScene(scene);
-	      
-	      primaryStage.show();
+		Rectangle recgul2 = new Rectangle();
+		rec.setTranslateY(50);
+		recgul2.setHeight(100);
+		recgul2.setWidth(600);
+		recgul2.setFill(Color.YELLOW);
+		recgul2.setY(200);
+
+		HBox box = new HBox();
+
+		Group root = new Group(rec, recgul1, recgul2);
+
+		box.getChildren().add(root);
+
+		Scene scene = new Scene(box, 600, 300);
+
+		stage.setTitle("Drawing a Germanny");
+
+		stage.setScene(scene);
+
+		stage.show();
+	}
+
+	public static void frankrike() {
+		Rectangle rec = new Rectangle();
+		rec.setTranslateY(50);
+		rec.setHeight(300);
+		rec.setWidth(200);
+		rec.setFill(Color.BLUE);
+
+		Rectangle recgul1 = new Rectangle();
+		rec.setTranslateY(50);
+		recgul1.setHeight(300);
+		recgul1.setWidth(200);
+		recgul1.setFill(Color.WHITE);
+		recgul1.setX(300);
+
+		Rectangle recgul2 = new Rectangle();
+		rec.setTranslateY(50);
+		recgul2.setHeight(300);
+		recgul2.setWidth(200);
+		recgul2.setFill(Color.RED);
+		recgul2.setX(400);
+
+		HBox box = new HBox();
+
+		Group root = new Group(rec, recgul1, recgul2);
+
+		box.getChildren().add(root);
+
+		Scene scene = new Scene(box, 600, 300);
+
+		stage.setTitle("Drawing a France");
+
+		stage.setScene(scene);
+
+		stage.show();
+	}
+
+	public static void schewiz() {
+		Rectangle rec = new Rectangle();
+		rec.setTranslateY(50);
+		rec.setHeight(300);
+		rec.setWidth(600);
+		rec.setFill(Color.RED);
+
+		Rectangle recgul1 = new Rectangle();
+		rec.setTranslateY(50);
+		recgul1.setHeight(160);
+		recgul1.setWidth(50);
+		recgul1.setFill(Color.WHITE);
+		recgul1.setX(275);
+		
+
+		Rectangle recgul2 = new Rectangle();
+
+		recgul2.setHeight(50);
+		recgul2.setWidth(160);
+		recgul2.setFill(Color.WHITE);
+		rec.setTranslateY(50);
+		recgul2.setX(220);
+
+		HBox box = new HBox();
+
+		Group root = new Group(rec, recgul1, recgul2);
+
+		box.getChildren().add(root);
+
+		Scene scene = new Scene(box, 600, 300);
+
+		stage.setTitle("Drawing a Switzerland");
+
+		stage.setScene(scene);
+
+		stage.show();
+	}
+
+	public static void sverige() {
+
+		Rectangle rec = new Rectangle();
+		rec.setTranslateY(50);
+		rec.setHeight(300);
+		rec.setWidth(600);
+		rec.setFill(Color.BLUE);
+
+		Rectangle recgul1 = new Rectangle();
+		rec.setTranslateY(50);
+		recgul1.setHeight(300);
+		recgul1.setWidth(58);
+		recgul1.setFill(Color.YELLOW);
+		recgul1.setX(150);
+
+		Rectangle recgul2 = new Rectangle();
+		rec.setTranslateY(50);
+		recgul2.setHeight(58);
+		recgul2.setWidth(600);
+		recgul2.setFill(Color.YELLOW);
+		
+
+		HBox box = new HBox();
+
+		Group root = new Group(rec, recgul1, recgul2);
+
+		box.getChildren().add(root);
+
+		Scene scene = new Scene(box, 600, 300);
+
+		stage.setTitle("Drawing a Sweden");
+
+		stage.setScene(scene);
+
+		stage.show();
+	}
+	public void frankrikeknappen(Stage knapp){
+		frankrike.setMinHeight(30);
+		frankrike.setMinWidth(30);
+		frankrike.setOnAction(this);
+		
+		Group knappen = new Group();
+		knappen.setTranslateX(300);
+		knappen.getChildren().add(frankrike);
+		
+		Scene scene = new Scene(knappen, 600, 500);
+		knapp.setScene(scene);
+		knapp.show();
+		
+		
+		
+	}
+	public void schweizkanppen(Stage knapp){
+		schewiz.setMinHeight(30);
+		schewiz.setMinWidth(30);
+		schewiz.setOnAction(this);
+		
+		Group knappen = new Group();
+		knappen.setTranslateX(300);
+		knappen.getChildren().add(schewiz);
+		
+		Scene scene = new Scene(knappen, 600, 500);
+		knapp.setScene(scene);
+		knapp.show();
+		
+		
+		
+	}
+	public void sverigeknappen(Stage knapp){
+		sverige.setMinHeight(30);
+		sverige.setMinWidth(30);
+		sverige.setOnAction(this);
+		
+		Group knappen = new Group();
+		knappen.setTranslateX(300);
+		knappen.getChildren().add(sverige);
+		
+		Scene scene = new Scene(knappen, 600, 500);
+		knapp.setScene(scene);
+		knapp.show();
+		
+		
+		
+	}
+	public void tysklandknppen(Stage knapp){
+		tyskland.setMinHeight(30);
+		tyskland.setMinWidth(30);
+		tyskland.setOnAction(this);
+		
+		Group knappen = new Group();
+		knappen.setTranslateX(300);
+		knappen.getChildren().add(tyskland);
+		
+		Scene scene = new Scene(knappen, 600, 500);
+		knapp.setScene(scene);
+		knapp.show();
+		
+		
+		
 	}
 
 	@Override
 	public void handle(ActionEvent event) {
-		if (event.getSource() == knappen)
-		{
-			  
+		if (event.getSource() == sverige) {
+			sverige();
+		} else if (event.getSource() == tyskland) {
+			tyskland();
+		} else if (event.getSource() == schewiz) {
+			schewiz();
+		} else if (event.getSource() == sverige) {
+			frankrike();
 		}
-		
+
 	}
 
 }
